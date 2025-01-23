58e06380fc01f7e2f805612504af396612979a18
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-01-23 17:36:53 +0800

    feat: add models for Molecule, Category, and Chirality; update settings and admin registration

--- backend/proteins/admin.py
+++ backend/proteins/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Category, Molecule, Chirality
+
 # Register your models here.
+admin.site.register(Molecule)
+admin.site.register(Category)
+admin.site.register(Chirality)
--- backend/proteins/models.py
+++ backend/proteins/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
-# Create your models here.
+
+# Define the Category model to store the category of the molecule
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# Define the Chirality model to store the chirality of the molecule
+class Chirality(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Molecule(models.Model):
+    IUPAC_NAME_MAX_LENGTH = 200
+    SMILE_MAX_LENGTH = 200
+    MOLECULE_FORMULA_MAX_LENGTH = 100
+
+    name = models.CharField(
+        max_length=IUPAC_NAME_MAX_LENGTH
+    )  # explicitly set max_length to 200 in case of long names
+
+    cas_id = models.CharField(max_length=20, unique=True)  # this should be unique
+
+    category = models.ManyToManyField(
+        Category, related_name="molecules"
+    )  # category of the molecule
+
+    url = models.URLField(blank=True, null=True)
+    pubchem_url = models.URLField(
+        blank=True, null=True
+    )  # https://pubchem.ncbi.nlm.nih.gov/compound/46939810
+
+    smiles = models.CharField(max_length=SMILE_MAX_LENGTH)
+
+    chirality = models.ManyToManyField(
+        Chirality, related_name="molecules"
+    )  # smile type of the molecule
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )  # [optional] any additional information about the molecule
+
+    pubchem_cid = models.CharField(
+        max_length=20, blank=True, null=True
+    )  # this should be unique
+
+    smiles_iupac = models.CharField(
+        max_length=IUPAC_NAME_MAX_LENGTH, blank=True, null=True
+    )  # IUPAC name of the molecule
+
+    molecule_formula = models.CharField(
+        max_length=MOLECULE_FORMULA_MAX_LENGTH,
+        blank=True,
+        null=True,
+    )  # C19H23O3P
+
+    molecular_weight = models.FloatField(blank=True, null=True)  # 330.364
+
+    InChI = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    InChIKey = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    # Calculated statistics
+    heavy_atom_count = models.IntegerField(blank=True, null=True)  # 23
+    ring_count = models.IntegerField(blank=True, null=True)  # 2
+    hydrogen_bond_acceptor_count = models.IntegerField(blank=True, null=True)  # 3
+    hydrogen_bond_donor_count = models.IntegerField(blank=True, null=True)  # 0
+    rotatable_bond_count = models.IntegerField(blank=True, null=True)  # 2
+
+    # Thermodynamic properties
+    zero_point_correction = models.FloatField(blank=True, null=True)
+    thermal_correction_energy = models.FloatField(blank=True, null=True)
+    thermal_correction_enthalpy = models.FloatField(blank=True, null=True)
+    thermal_correction_gibbs = models.FloatField(blank=True, null=True)
+    sum_electronic_zero_point = models.FloatField(blank=True, null=True)
+    sum_electronic_thermal_energy = models.FloatField(blank=True, null=True)
+    sum_electronic_thermal_enthalpy = models.FloatField(blank=True, null=True)
+    sum_electronic_thermal_free_energy = models.FloatField(blank=True, null=True)
+
+    # Molecular orbital properties
+    homo_energy = models.FloatField(blank=True, null=True)
+    lumo_energy = models.FloatField(blank=True, null=True)
+    homo_lumo_gap = models.FloatField(blank=True, null=True)
